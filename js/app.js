fb9b79e0b1ffb38f8c3481841e836c4549a5f127
hann013 <hannah.zhang813@gmail.com>
2015-05-09 04:12:40 -0400

    Extension homepage with clock, date, and weather; configured getting automatic location through geolocation, set up routing to settings page

---
+++ js/app.js
@@ -0,0 +1,101 @@
+var app = angular.module("CurrentlyApp", ['ngRoute']);
+
+// Display and update the time and date
+app.controller("ClockController", function($scope) {
+    // Function that updates clock time
+    function updateClock() {
+        $scope.clock = new Date();
+    }
+
+    // Initialize time on load
+    updateClock();
+
+    // Update clock time every minute and bind change in time
+    var timer = setInterval(function() {
+        $scope.$apply(updateClock);
+    }, 1000);
+});
+
+app.controller("WeatherController", function($scope, WeatherForecast) {
+    $scope.weather = {};
+
+    navigator.geolocation.getCurrentPosition(setPosition, errorHandler);
+
+    function setPosition(position) {
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
+        var userLocation = latitude + "," + longitude;
+
+        WeatherForecast.getWeatherForecast(userLocation)
+        .then(function(data){
+            $scope.userLocation = data[0];
+            $scope.weather.forecast = data[1];
+        });
+    }
+
+    function errorHandler(err) {
+        console.log(err);
+    }
+});
+
+// Display weather information
+app.provider("WeatherForecast", function() {
+    var apiKey = "";
+
+    this.setApiKey = function(key)
+    {
+        if (key) { this.apiKey = key; } 
+    }
+
+    this.createRequestUrl = function(type, location) {
+        return "http://api.wunderground.com/api/" + this.apiKey + "/" + type + "/q/" + location + ".json";
+    }
+
+    this.$get = function($q, $http) {
+        var self = this;
+
+        return {
+            getWeatherForecast: function(location) {
+                var d = $q.defer();
+
+                $http({
+                    method: "GET",
+                    url: self.createRequestUrl("geolookup/forecast", location),
+                    cache: true
+                })
+                .success(function(data) {
+                    d.resolve([data.location, data.forecast.simpleforecast]);
+                })
+                .error(function(err) {
+                    d.reject(err);
+                });
+
+                return d.promise;
+            }
+        }
+    }
+});
+
+app.controller("SettingsController", function($scope) {
+
+});
+
+// Set Wunderground API key - withheld from GitHub
+app.config(function(WeatherForecastProvider) {
+    WeatherForecastProvider.setApiKey("");
+});
+
+// Routing to multiple screens
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl: "/templates/home.html",
+    })
+    .when("/settings", {
+        templateUrl: "/templates/settings.html",
+        controller: "SettingsController"
+    })
+    .otherwise({
+        redirectTo: "/"
+    });
+});
